b06f8c950a1c74da20f8db47460649d546db5120
Zeel Modi <zeel.m@simformsolutions.com>
2024-06-04 13:08:47 +0530

    feat: server side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,41 +1,17 @@
-'use client'
-
 import NewsList from '@/components/NewsList';
-import { useEffect, useState } from 'react';
 
 
-export default function NewsPage() {
+export default async function NewsPage() {
 
-    const [error, setError] = useState()
-    const [isLoading, setIsLoading] = useState(false)
-    const [news, setNews] = useState([])
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/news`);
 
-    useEffect(() => {
-        
-        async function fetchNews() {
-            setIsLoading(true)
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/news`);
-
-            if (!response.ok) {
-                setError('Failed to fetch news.')
-                setIsLoading(false)
-            }
-            const news = await response.json();
-            setNews(news)
-            setIsLoading(false)
-
-        }
-
-        fetchNews()
-    }, [])
-
-    if (isLoading) {
-        return <p>Loading...</p>
+    if (!response.ok) {
+        throw new Error('Failed to fetch news!')
     }
+    
+    const news = await response.json();
 
-    if (error) {
-        return <p>{error}</p>
-    }
+  
     
     let newsContent;
     if (news.length) {
